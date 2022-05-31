d335bc148d77b1af9cd9359a61f5a59ade11a1df
pafo-10 <pedrofigueredo1029@gmail.com>
2022-05-31 14:38:27 -0500

    se agrega el proyecto

---
+++ conexion.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Solicitud de carnet</title>
+</head>
+<body>
+    
+    <header>
+        <h2>SOLICITAR CARNET</h2>
+    </header>
+
+    <section class="botones">
+        <div class="btn-volver">
+            <a href="http://127.0.0.1:5500/Men%C3%BA_Carnet.html"><input class="boton-volver" type="submit" name="" value="Volver"></a>
+        </div>
+        <div class="btn-salir">
+            <a href="http://127.0.0.1:5500/login.html#"><input class="boton-salir" type="submit" name="" value="Salir"></a>
+        </div>
+    </section>
+
+    <section class="caja-img">
+        <img class="escudo" src="Imagenes/escudo.png">
+    </section>
+
+    <section class="form-login4">
+        <div class="container-tabla">
+            <h2>HORARIOS</h2>
+            <div class="listado-tabla">
+                <!--acá va la tabla de horarios en solicitar carnet-->
+                <!--ACÁ NO HAY NADA EN ESPECIFICO Y NO HAY CONEXIÓN DE BASES DE DATOS AÚN-->
+            </div>
+        </div>
+    </section>
+
+<footer class="footerid">
+    <div class="container">
+        <h2>Redes sociales</h2>
+        <p>Solicitud de carnet para sistema de carnetización de la Univerdad del Magdalena, todos los derechos reservados</p>
+        <ul class="social">
+            <li><a href="https://es-la.facebook.com/UniversidadDelMagdalena/"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://twitter.com/Unimagdalena?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCE9z8A03zoRvuMVq1LPINTA"><i class="fa fa-youtube"></i></a></li>
+            <li><a href="https://www.linkedin.com/school/unimagdalena/?originalSubdomain=co"><i class="fa fa-linkedin-square"></i></a></li>
+        </ul>
+    </div>
+</footer>
+
+</body>
+</html>
